defce0898b11293d7648670dad79d517ee5253c8
Stanislav <stanislav.titarenko@gmail.com>
2024-09-30 00:39:11 +0300

    24.2 Сериализаторы

--- dogs/serializers.py
+++ dogs/serializers.py
@@ -1,9 +1,29 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from dogs.models import Dog, Breed
 
 
+class BreedSerializer(ModelSerializer):
+    dogs = SerializerMethodField()
+
+    def get_dogs(self, breed):
+        # получаем все собаки этой породы
+        # return Dog.objects.filter(breed=breed)  # obj или breed это экземпляр порода, по которому ищем собаки
+
+        # можно также сделать return имен собак.
+        # делаем список и из перебора выбираем только имя.
+        return [dog.name for dog in Dog.objects.filter(breed=breed)]
+
+    class Meta:
+        model = Breed
+        # определяет какие поля нам будут возвращаться в ответе 200 (ОК)
+        fields = '__all__'
+
+
 class DogSerializer(ModelSerializer):
+    breed = BreedSerializer(read_only=True)
+
     class Meta:
         model = Dog
         # поля, которые будут задействованы из модели.
@@ -12,8 +32,21 @@ class DogSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class BreedSerializer(ModelSerializer):
+class DogDetailSerializer(ModelSerializer):
+    # нужно добавить поле, которое будет выводить собак такой же породы
+    # если нам нужно добавить кастомное поле, нужно его прописать
+    # но у нас не описан метод, как получать это поле, описываем ниже (def get_count_dog_with_same_breed)
+    count_dog_with_same_breed = SerializerMethodField()
+
+    # теперь поле breed будет определяться сериализатором BreedSerializer
+    breed = BreedSerializer(read_only=True)  # включаем сериализатор порода в ответе.
+
+    # метод прописываем как get и название поля выше.
+    # передаем в него obj, instance Или dog как в тек.примере (неважно), т.к. класс по собаке
+    def get_count_dog_with_same_breed(self, dog):
+        # берем породу и фильтруем всех собак по этой породе.
+        return Dog.objects.filter(breed=dog.breed).count()
+
     class Meta:
-        model = Breed
-        # определяет какие поля нам будут возвращаться в ответе 200 (ОК)
-        fields = '__all__'
+        model = Dog
+        fields = ('name', 'breed', 'date_born', 'count_dog_with_same_breed',)
--- dogs/views.py
+++ dogs/views.py
@@ -1,14 +1,31 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from dogs.models import Dog, Breed
-from dogs.serializers import DogSerializer, BreedSerializer
+from dogs.serializers import DogSerializer, BreedSerializer, DogDetailSerializer
 
 
 class DogViewSet(ModelViewSet):
     queryset = Dog.objects.all()
+    # добавляем сортировку
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    # указываем поля для фильтрации из django-filters (DjangoFilterBackend)
+    # меняем список на кортеж [] -> ()
+    filterset_fields = ('breed',)
+    # добавялем поля для сортировки
+    ordering_fields = ('date_born',)
+    search_fields = ('name',)
+
     # Сериализаторы нужны, чтобы обрабатывать данные из БД в формат, который мы будем выводить
-    serializer_class = DogSerializer
+    # serializer_class = DogSerializer
+
+    # переопределяем и проверяем. Если у нас действие retrieve, то мы выводим DogDetailSerializer
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return DogDetailSerializer
+        return DogSerializer
 
 
 # Делаем CRUD классы для пород
